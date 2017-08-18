7436b8966ce47ab79834471f433515d611038bf3
krishna Minnamareddy <krishnavikas@ccs.neu.edu>
2017-08-18 08:19:12 -0400

    saving mongo uri

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connectionString = 'mongodb://vikas:vikas@ds151242.mlab.com:51242/heroku_g0sv4lv3';
+const connectionString = process.env.MONGODB_URI;
 
 mongoose.connect(connectionString, { useMongoClient: true });
 mongoose.connection;
